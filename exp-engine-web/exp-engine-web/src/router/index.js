c80dc7767fc3701ecc7b5bf904e4df5945d07998
ant-black <1016930479@qq.com>
2019-11-20 23:13:00 +0800

    add

--- exp-engine-web/exp-engine-web/src/router/index.js
+++ exp-engine-web/exp-engine-web/src/router/index.js
@@ -1,15 +1,24 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+
+
+
+import index from '../page/index.vue'
+import user from '../page/user.vue'
 
 Vue.use(Router)
-
+ 
 export default new Router({
   routes: [
     {
-      path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
-    }
+        path: '/',
+        name: 'index',
+        component: index
+      },
+      {
+        path: '/user',
+        name: 'user',
+        component: user
+      },
   ]
 })

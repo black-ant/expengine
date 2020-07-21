aa2e8ef3089088807f5fd039b9b4038672b13996
black-ant <1016930479@qq.com>
2020-07-21 22:19:13 +0800

    add

---
+++ engine-api/src/main/java/com/gang/etl/engine/api/common/IPluginsService.java
@@ -0,0 +1,10 @@
+package com.gang.etl.engine.api.common;
+
+/**
+ * @Classname IPluginsService
+ * @Description TODO
+ * @Date 2020/7/21 22:10
+ * @Created by zengzg
+ */
+public interface IPluginsService {
+}
---
+++ engine-support/support-velocity/src/main/java/com/gang/etl/velocity/VelocityProduceService.java
@@ -0,0 +1,13 @@
+package com.gang.etl.velocity;
+
+import org.springframework.stereotype.Component;
+
+/**
+ * @Classname VelocityService
+ * @Description TODO
+ * @Date 2020/7/21 22:13
+ * @Created by zengzg
+ */
+@Component
+public class VelocityProduceService implements IProduceService {
+}
